247e0df19d07ca018e240360fd8bb88f95353a63
Konstantin Terekhin <konstantin.teryohin@rubius.com>
2020-05-24 16:30:37 +0700

    added db-in-memory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [  ],
   controllers: [AppController],
   providers: [AppService],
 })
